df0e1b8fc206ea1105405c00ac885387d949b074
Noah Zweben <noahzweben@gmail.com>
2023-02-20 14:38:17 +0000

    fixed benchmark

--- benchmark.c
+++ benchmark.c
@@ -21,7 +21,7 @@ void shuffle_list(node *random_array, int size)
 int main(int argc, char **argv)
 {
   // setup benchmark
-  lsmtree *lsm = create(10);
+  lsmtree *lsm = create(BLOCK_SIZE_NODES);
 
   int seed = 2;
   srand(seed);
